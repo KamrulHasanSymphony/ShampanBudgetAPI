337cfc50d4eb8ba47cdbc28cb325957610e9877b
DEV-1019\Monir <Monir.Hossen@symphonysoftt.com>
2025-11-30 10:17:08 +0600

    Deparment sabre Dropdown

--- ShampanBFRS.ViewModel/SetUpVMs/SabresVM.cs
+++ ShampanBFRS.ViewModel/SetUpVMs/SabresVM.cs
@@ -12,7 +12,7 @@ namespace ShampanBFRS.ViewModel.SetUpVMs
     {
         public int Id { get; set; }
 
-        [Display(Name = "COA ID")]
+        [Display(Name = "COA")]
         public int? COAId { get; set; }
 
         [Display(Name = "Code")]
